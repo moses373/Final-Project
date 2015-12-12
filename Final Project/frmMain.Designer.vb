e0522b4a44c00fa2a9b33b37cee7ada1f89af5f5
moses373 - Eric Moses <moses373@gmail.com>
2015-12-12 16:06:29 -0700

    first commit

---
+++ Final Project/frmMain.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtOdometer = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblTrip = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblPrevious = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtOdometer
+        '
+        Me.txtOdometer.Location = New System.Drawing.Point(262, 149)
+        Me.txtOdometer.Name = "txtOdometer"
+        Me.txtOdometer.Size = New System.Drawing.Size(100, 20)
+        Me.txtOdometer.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Final_Project.My.Resources.Resources.odometer
+        Me.PictureBox1.Location = New System.Drawing.Point(62, 59)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(128, 110)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(266, 133)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Odometer Reading"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(62, 306)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(128, 44)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(249, 306)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(128, 44)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotal.Location = New System.Drawing.Point(169, 249)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotal.TabIndex = 5
+        '
+        'lblTrip
+        '
+        Me.lblTrip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTrip.Location = New System.Drawing.Point(276, 249)
+        Me.lblTrip.Name = "lblTrip"
+        Me.lblTrip.Size = New System.Drawing.Size(100, 23)
+        Me.lblTrip.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(139, 13)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(152, 26)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Trip Calculator"
+        '
+        'lblPrevious
+        '
+        Me.lblPrevious.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblPrevious.Location = New System.Drawing.Point(62, 249)
+        Me.lblPrevious.Name = "lblPrevious"
+        Me.lblPrevious.Size = New System.Drawing.Size(100, 23)
+        Me.lblPrevious.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 236)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Previous Reading:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(168, 236)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Current Total:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(273, 236)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Trip:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(439, 414)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lblPrevious)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lblTrip)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtOdometer)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtOdometer As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents lblTrip As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents lblPrevious As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Final Project/frmMain.vb
@@ -0,0 +1,47 @@
+﻿
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmMain_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        Dim lines() As String = IO.File.ReadAllLines("Previous.txt")
+        lblPrevious.Text = lines(lines.Length - 1)
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        ' declare variables and assign inputs
+        Dim intOdometer As Integer
+        Dim intPrevious As Integer
+        Dim intTotal As Integer
+        Dim intTrip As Integer
+        Dim lines() As String = IO.File.ReadAllLines("Previous.txt")
+        Dim outPrevious As IO.StreamWriter
+        Integer.TryParse(txtOdometer.Text, intOdometer)
+
+        ' get previous odometer reading
+        intPrevious = lines(lines.Length - 1)
+        lblPrevious.Text = intPrevious
+        ' get current Odometer Reading and calculate and display trip miles. Also updates Previous.txt files
+        ' to the new value
+        If intOdometer >= lines(lines.Length - 1) Then
+            lblTotal.Text = intOdometer
+            intTrip = intOdometer - intPrevious
+            lblTrip.Text = intTrip
+        Else
+            MessageBox.Show("Please enter number greater than previous odometer reading", "Current Odometer = " & lines.Length - 1)
+            intTotal = intOdometer
+        End If
+
+        If intOdometer >= lines(lines.Length - 1) AndAlso IO.File.Exists("Previous.txt") = True Then
+            outPrevious = IO.File.AppendText("Previous.txt")
+            outPrevious.WriteLine(ControlChars.NewLine & lblTotal.Text.Trim)
+            outPrevious.Close()
+            txtOdometer.Text = String.Empty
+            txtOdometer.Focus()
+        End If
+    End Sub
+
+End Class
